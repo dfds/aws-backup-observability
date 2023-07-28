50d55f3ba1526c447f69e0cb3a8604ab747f52ad
Aleksandra Korczak <alekorc@dfds.com>
2023-07-28 12:52:08 +0200

    Add backup observability infrastructure

---
+++ backup-report-plan.tf
@@ -0,0 +1,20 @@
+data "aws_organizations_organization" "this" {}
+
+resource "aws_backup_report_plan" "this" {
+  name = "backup-jobs-organization-report"
+  description = "Report plan to aggregate results of backup jobs across the organization"
+  report_delivery_channel {
+    s3_bucket_name = module.reports_bucket.bucket_name
+    formats = ["CSV"]
+  }
+  report_setting {
+    report_template = "BACKUP_JOB_REPORT"
+    organization_units = [
+      data.aws_organizations_organization.this.accounts
+    ]
+    regions = [
+      "eu-central-1",
+      "eu-west-1"
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "slack_webhook_url" {
+  type = string
+  description = "Slack webhook URL used to post backup reports"
+}
