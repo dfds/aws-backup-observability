1de892d91e200d170b95c80bbb1f1eba9ec1c387
Aleksandra Korczak <alekorc@dfds.com>
2023-08-15 15:41:03 +0200

    Fix errors

--- backup-report-plan.tf
+++ backup-report-plan.tf
@@ -9,7 +9,7 @@ resource "aws_backup_report_plan" "this" {
   }
   report_setting {
     report_template = "BACKUP_JOB_REPORT"
-    organization_units = data.aws_organizations_organization.this.accounts
+    organization_units = [for account in data.aws_organizations_organization.this.non_master_accounts : account.id ]
     regions = [
       "eu-central-1",
       "eu-west-1"
