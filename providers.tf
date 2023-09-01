190889bb6b1a3ad1a0cd0ec1dc0d2b1bc46cf4f9
Aleksandra Korczak <61739075+aleksandra-korczak@users.noreply.github.com>
2023-09-01 14:02:27 +0200

    Revert "Remove assume_role"

--- providers.tf
+++ providers.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = "eu-central-1"
+  assume_role {
+    role_arn = var.aws_assume_role_arn
+  }
   default_tags {
     tags = var.default_tags
   }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "aws_assume_role_arn" {
+  type = string
+  description = "ARN of the role to assume by the provider"
+  default = ""
+}
+
 variable "default_tags" {
   type = object({})
   description = "A map of default tags that will be applied to all resources that support tagging"
