17b0a6d5a3627347aff10709b12a78f7033be270
Aleksandra Korczak <61739075+aleksandra-korczak@users.noreply.github.com>
2023-09-05 11:32:01 +0200

    Fix lifecycle rule name

--- s3-bucket-backup-reports.tf
+++ s3-bucket-backup-reports.tf
@@ -2,7 +2,7 @@ locals {
   bucket_name = "dfds-backup-reports"
   lifecycle_configuration = [
     {
-      id     = "delete-after=1-day"
+      id     = "delete-after-30-days"
       status = "Enabled"
       expiration = {
         days = 30
