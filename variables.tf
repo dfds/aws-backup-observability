2e4bc02d278e6a7fe21dfc383bab2b9f12cf5489
Silviu Calin <silviuandrei.calin@gmail.com>
2024-04-16 11:51:55 +0200

    Needed components to be deployed in the compliance account.

--- variables.tf
+++ variables.tf
@@ -15,3 +15,53 @@ variable "slack_webhook_url" {
   description = "Slack webhook URL used to post backup reports"
   sensitive   = true
 }
+
+variable "aws_region" {
+  type = string
+  default = "eu-central-1"
+}
+
+variable "project_name" {
+    type = string
+}
+
+variable "bucket_name" {
+}
+
+variable "bucket_force_delete" {
+    default = false
+}
+
+
+variable "k8s_namespace" {
+
+}
+
+variable "k8s_service_account" {
+
+}
+
+variable "oidc_endpoint_kubernetes" {
+  type = list(string)
+  description = "The OIDC endpoint for the EKS cluster"  
+}
+
+variable "oidc_eks_for_athena" {
+  type = string
+  description = "The OIDC endpoint for the EKS cluster"  
+}
+
+variable "grafana_stack_ids" {
+  type = list(string)
+  description = "value"
+}
+
+variable "grafana_cloud_arn" {
+  type = list(string)
+  description = "The ARN of Grafana Cloud connection."
+}
+
+variable "athena_db_name" {
+  type = string
+  description = "The name of the Athena database which is created by AWS Glue"
+}
